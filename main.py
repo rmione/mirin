1a4f8bfaea68634d15679914fa9a2cdaf7355599
Riley Mione <rmione33@gmail.com>
2020-08-07 19:07:14 -0400

    initial commit!

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+import json 
+
+"""
+For now we are going to use Romaji titles for the anime and search the database that we are going to use (for now, kitsuneko.)
+Kitsuneko uses a pretty simplistic URL structure so I think that this will be pretty simple to work with. Let's get going! 
+"""
+
+pass 
