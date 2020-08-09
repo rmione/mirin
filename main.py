0eb7804525098e25e9e84c9eb80c1cbf01413519
Riley Mione <rmione33@gmail.com>
2020-08-08 21:49:46 -0400

    fixed errors in the extracting of the subtitle archives

--- main.py
+++ main.py
@@ -4,18 +4,13 @@ import zipfile
 """
 This module will deal with searching the kanji and maybe making the cards
 """
-print(os.listdir())
-# with open(os.getcwd())
-# for file in os.listdir():
-#     print(file)
-#     fn = str(file).split('.')[1]
-#     print(fn[1])
-#     if not fn[1] in ['zip', 'rar']:
-#         continue 
-#     # Otherwise it's a zipfile! 
-#     with zipfile.ZipFile(os.getcwd() + file, 'r') as subtitle_archive: 
-#         subtitle_archive.extractall("./extracted/{}".format(fn[0]))
-fn = ["stuff"]
-with zipfile.ZipFile('./stuff.zip', 'r') as subtitle_archive: 
-    subtitle_archive.extractall("./extracted/{}".format(fn[0]))
-        
+
+for file in os.listdir():
+    fn = file.split('.')
+    if not fn[1] in ['zip', 'rar']:
+        continue 
+   
+    # Otherwise it's a zipfile! 
+    with zipfile.ZipFile('./{0}'.format(file), 'r') as subtitle_archive: 
+        subtitle_archive.extractall("./extracted/{}".format(fn[0]))
+
