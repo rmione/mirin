e68a73e65b5892085b3c980d421f645506146400
Riley Mione <rmione33@gmail.com>
2020-08-09 16:31:10 -0400

    Continued refactoring, some comments addeD

--- search.py
+++ search.py
@@ -5,9 +5,12 @@ import json
 base_url = "https://kanjiapi.dev/v1/kanji/堂"
 kanji = "堂"
 
-encoded = base_url.encode('utf-8')
+encoded = base_url.encode('utf-8') # This looks like b'https://kanjiapi.dev/v1/kanji/\xe5\xa0\x82' etc
+print(encoded)
 decoded = encoded.decode('utf-8')
 response = requests.get(decoded)
 print(response.status_code)
+print(type(json.dumps(response.json())))
 with open("./testing_response.json", 'w+') as f:
+    
     json.dump(response.json(), f)
