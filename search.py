2c9d3d8a52a80dbe941b799b3cabf5dd1adf405f
Riley Mione <rmione33@gmail.com>
2020-08-09 15:29:17 -0400

    taking a slightly different approach this time around

---
+++ search.py
@@ -0,0 +1,10 @@
+import requests
+# -*- coding: utf-8 -*-
+
+base_url = "https://kanjiapi.dev/v1/kanji/堂"
+test = 'https://kanjialive-api.p.rapidapi.com/api/public/kanji/'
+kanji = "堂"
+
+encoded = base_url.encode('utf-8')
+# decoded = encoded.decode('utf-8', errors='ignore')
+decoded = encoded.decode('utf-8')
