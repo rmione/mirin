b28a95d583fbd93ddd716f9d652d75bfb4968b98
Riley Mione <rmione33@gmail.com>
2020-08-08 21:59:17 -0400

    Cleaned up code, now uses os.path.isfile to distinguish whether or not it's a file or directory!

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ import zipfile
 """
 This module will deal with searching the kanji and maybe making the cards
 """
+def extract_subs(): 
+    for file in os.listdir(): 
+        if not os.path.isfile(file): 
+            continue 
 
-for file in os.listdir():
-    fn = file.split('.')
-    if not fn[1] in ['zip', 'rar']:
-        continue 
-   
-    # Otherwise it's a zipfile! 
-    with zipfile.ZipFile('./{0}'.format(file), 'r') as subtitle_archive: 
-        subtitle_archive.extractall("./extracted/{}".format(fn[0]))
+        fn = file.split('.')
+        if not fn[1] in ['zip', 'rar']:
+            continue 
+    
+        # Otherwise it's a zipfile! 
+        with zipfile.ZipFile('./{0}'.format(file), 'r') as subtitle_archive: 
+            subtitle_archive.extractall("./extracted/{}".format(fn[0]))
 
+extract_subs()
