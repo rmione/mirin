2bf5c816232f6d7ecc7e669f8d55fe1095775280
Riley Mione <rmione33@gmail.com>
2020-08-08 23:08:11 -0400

    Hashing out this stuff with encoding, I think I'm getting somewhere...

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import srt
 import os 
 import zipfile
 import pysrt 
+import sys
 """
 This module will deal with searching the kanji and maybe making the cards
 """
+print(sys.getdefaultencoding())
 def extract_subs(): 
     for file in os.listdir(): 
         if not os.path.isfile(file): 
@@ -21,8 +23,7 @@ def handle_srt():
     for subtitle in os.listdir('./extracted/stuff'):
         print(subtitle)
         subs = pysrt.open('./extracted/stuff/'+subtitle, encoding='utf-8-sig')
-        print(str(subs[0]).encode('utf-8-sig'))
-            
-extract_subs()
+        print(subs[0].text.encode('utf-8'))    
+# extract_subs()
 handle_srt()
-
+print(sys.stdout.encoding)
