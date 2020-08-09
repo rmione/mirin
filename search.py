0165154c4f406564106f13b7ba4f1422e55d48de
Riley Mione <rmione33@gmail.com>
2020-08-09 16:59:42 -0400

    '

--- search.py
+++ search.py
@@ -2,15 +2,15 @@ import requests
 import json 
 # -*- coding: utf-8 -*-
 
-base_url = "https://kanjiapi.dev/v1/kanji/堂"
-kanji = "堂"
+BASE_URL = "https://kanjiapi.dev/v1/kanji/"
 
-encoded = base_url.encode('utf-8') # This looks like b'https://kanjiapi.dev/v1/kanji/\xe5\xa0\x82' etc
-print(encoded)
-decoded = encoded.decode('utf-8')
-response = requests.get(decoded)
-print(response.status_code)
-print(type(json.dumps(response.json())))
-with open("./testing_response.json", 'w+') as f:
-    
-    json.dump(response.json(), f)
+def search_kanji(kanji):
+    encoded = BASE_URL.encode('utf-8') # This looks like b'https://kanjiapi.dev/v1/kanji/\xe5\xa0\x82' etc
+    print(encoded)
+    decoded = encoded.decode('utf-8')
+    response = requests.get(decoded)
+    print(response.status_code)
+    print(type(json.dumps(response.json())))
+    with open("./testing_response.json", 'w+') as f:
+        
+        json.dump(response.json(), f)
