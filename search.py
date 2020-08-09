af6450854c872734729cc008ce59868cb6a479da
Riley Mione <rmione33@gmail.com>
2020-08-09 15:33:37 -0400

    Due to standard out errors, i can't really print it which is fine. Encoding is being tough. It is getting a response now, though

--- search.py
+++ search.py
@@ -1,10 +1,13 @@
 import requests
+import json 
 # -*- coding: utf-8 -*-
 
 base_url = "https://kanjiapi.dev/v1/kanji/堂"
-test = 'https://kanjialive-api.p.rapidapi.com/api/public/kanji/'
 kanji = "堂"
 
 encoded = base_url.encode('utf-8')
-# decoded = encoded.decode('utf-8', errors='ignore')
 decoded = encoded.decode('utf-8')
+response = requests.get(decoded)
+print(response.status_code)
+with open("./testing_response.json", 'w+') as f:
+    json.dump(response.json(), f)
