aa0baad020d9cef4c052c4378fd4641754fdc4e2
Riley Mione <rmione33@gmail.com>
2020-08-09 20:55:17 -0400

    Creation of very basic flashcards is working with Kanji, going to need to refactor majorly

---
+++ creator.py
@@ -0,0 +1,39 @@
+import genanki 
+import json 
+
+with open('./testing_response.json', 'r') as stream: 
+    data = json.load(stream)
+    model = genanki.Model(
+    1607392319,
+    'Simple Model',
+    fields=[
+    {'name': 'Question'},
+    {'name': 'Answer'},
+    ],
+    templates=[
+    {
+        'name': 'Card 1',
+        'qfmt': '<span class="kanji">{{Question}}</span>',
+        'afmt': '{{Answer}}',
+    } 
+    ],
+    css= """      .card {
+                font-family: mincho;
+                font-size: 88px;
+                text-align: center;
+                color: black;
+               }
+             .kanji {font-family: "Kozuka Mincho Pr6N"}
+             """
+    
+    )
+    my_note = genanki.Note(
+        model=model,
+        fields=[data["kanji"], 'Testing'])
+
+    my_deck = genanki.Deck(
+    2059400110,
+    'Test Kanji')
+
+    my_deck.add_note(my_note)
+    genanki.Package(my_deck).write_to_file('testKanji.apkg')
