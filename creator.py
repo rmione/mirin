4e7c6ffe89b9ece01be403eddaa179f61b43cd6b
Riley Mione <rmione33@gmail.com>
2020-08-11 21:39:02 -0400

    Moved the functionality to add a card to a function... further improving separation of concerns

--- creator.py
+++ creator.py
@@ -20,41 +20,50 @@ def load_config():
         MODEL_NO = data["model_number"]
         DECK_NO = data["deck_number"]
 
+model = genanki.Model(
+1607392319,
+'Simple Model',
+fields=[
+{'name': 'Kanji'},
+{'name': 'Meaning'},
+],
+templates=[
+{
+    'name': 'Card 1',
+    'qfmt': '<span class="kanji">{{Kanji}}</span>',
+    'afmt': '{{Answer}}',
+} 
+],
+css= """      .card {
+            font-family: mincho;
+            font-size: 88px;
+            text-align: center;
+            color: black;
+            }
+            .kanji {font-family: "Kozuka Mincho Pr6N"}
+            """
 
-load_config()
-with open('./testing_response.json', 'r') as stream: 
-    data = json.load(stream)
-    model = genanki.Model(
-    1607392319,
-    'Simple Model',
-    fields=[
-    {'name': 'Kanji'},
-    {'name': 'Meaning'},
-    ],
-    templates=[
-    {
-        'name': 'Card 1',
-        'qfmt': '<span class="kanji">{{Kanji}}</span>',
-        'afmt': '{{Answer}}',
-    } 
-    ],
-    css= """      .card {
-                font-family: mincho;
-                font-size: 88px;
-                text-align: center;
-                color: black;
-               }
-             .kanji {font-family: "Kozuka Mincho Pr6N"}
-             """
-    
-    )
-    my_note = genanki.Note(
-        model=model,
-        fields=[data["kanji"], 'Testing'])
+)
 
-    my_deck = genanki.Deck(
+ my_deck = genanki.Deck(
     2059400110,
     'Test Kanji')
 
+
+load_config()
+
+def add_card(deck, kanji):
+# with open('./testing_response.json', 'r') as stream: 
+    # data = json.load(stream)
+
+    # my_note = genanki.Note(
+    #     model=model,
+    #     fields=[data["kanji"], 'Testing'])
+
+    my_note = genanki.Note(
+        model=model,
+        fields=[kanji, 'Testing'])
+
+   
     my_deck.add_note(my_note)
     genanki.Package(my_deck).write_to_file('testKanji.apkg')
