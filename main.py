4f82560b0fe1c4a89efca04fc47239194cb52c5b
Riley Mione <rmione33@gmail.com>
2020-08-15 18:20:10 -0400

    Uses a dictionary as a database, and it seems fast enough, but I will need to test som

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import zipfile
 import pysrt 
 import sys
 import re
+import json
+import operator
 from mirin.search import Kanji
 """
 This module will deal with searching the kanji and maybe making the cards
@@ -32,15 +34,32 @@ def handle_srt():
     """
     for subdir in os.listdir('./extracted/'):
         print(subdir)
+        count = 0 
         for subtitle in os.listdir('./extracted/{}'.format(subdir)):
             subs = pysrt.open('./extracted/{0}/{1}'.format(subdir, subtitle), encoding='utf-8-sig')
             # print(str(subs[0].text.encode('utf-8')))
             # with open('./testing.txt', 'w+') as f: 
             #     f.write(subs[0].text.decode('utf-8'))
+            print("Subtitle " + subtitle)
+           
+            database = {}
+
+            # This is subtitle level
             for sub in subs: 
-                arr = sub.text[0]
-                print(Kanji.is_kanji(arr))
-                
+                arr = sub.text
+                # This is the sentence level
+                for char in arr:
+                    if Kanji.is_kanji(char):
+                        if not database.get(char):
+                            # Initialize database entry and zero count 
+                            database[char] = 0
+                        database[char]+=1
+                        # count +=1 
+            x = {k: v for k, v in sorted(database.items(), key=lambda item: item[1])}
+            with open('./kanji{}.json'.format(count), 'w+', encoding='utf8') as f: 
+                json.dump(x, f, ensure_ascii=False)
+            count += 1
+            # print(database)
 extract_subs()
 handle_srt()
 print(sys.stdout.encoding)
--- mirin/search.py
+++ mirin/search.py
@@ -36,7 +36,7 @@ class Kanji:
         """
         # regex = u"[\x3400-\x4DB5\x4E00-\x9FCB\xF900-\xFA6A]"
 
-        print(str(kanji.encode('utf-8')))
+        # print(str(kanji.encode('utf-8')))
         if re.match("([一-龯])", kanji):
             return True
         return False
