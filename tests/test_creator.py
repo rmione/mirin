ce77e5b5943dde5eb50f0bb799b747ac75d9dfeb
Riley Mione <rmione33@gmail.com>
2020-09-01 10:44:32 -0400

    added tests for creator

---
+++ tests/test_creator.py
@@ -0,0 +1,13 @@
+import unittest 
+import dashi.creator
+class TestCreator(unittest.TestCase): 
+    """
+    Test the methods in the creator module, not much to test but here goes.
+    """
+
+    def test_add_card():
+        pass
+    def test_config_yml():
+        pass
+    def test_dump_deck():
+        pass
