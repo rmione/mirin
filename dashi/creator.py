2c7adcd159d8941d0b977bcf394efabad57f556b
Riley Mione <rileymione@manjaro.cgocable.net>
2021-02-19 17:56:57 -0500

    format with black, refactor some code

--- dashi/creator.py
+++ dashi/creator.py
@@ -1,32 +1,29 @@
-import genanki 
-import json 
-import yaml 
+import genanki
+import json
+import yaml
 import os
-import random 
+import random
 import time
-import logging 
+import logging
 from genanki import Deck
 
 from dashi.search import Kanji
 
-CSS_STYLING = None # To test if this works
-if not os.path.isfile('./config.yml'): 
+CSS_STYLING = None  # To test if this works
+if not os.path.isfile("./config.yml"):
     MODEL_NO = random.randrange(1 << 30, 1 << 31)
     DECK_NO = random.randrange(1 << 30, 1 << 31)
-    with open('./config.yml', 'w+') as stream: 
-        data = {
-            "model_number": MODEL_NO,
-            "deck_number": DECK_NO
-        }
+    with open("./config.yml", "w+") as stream:
+        data = {"model_number": MODEL_NO, "deck_number": DECK_NO}
         yaml.safe_dump(data, stream)
-        
-else: 
-    with open('./config.yml', 'r') as stream:
+
+else:
+    with open("./config.yml", "r") as stream:
         data = yaml.safe_load(stream)
     MODEL_NO = data["model_number"]
     DECK_NO = data["deck_number"]
 
-    if not data.get('css_styling'):
+    if not data.get("css_styling"):
         CSS_STYLING = """      .card {
             font-family: mincho;
             font-size: 88px;
@@ -35,90 +32,91 @@ else:
             }
             .kanji {font-family: "Kozuka Mincho Pr6N"}
             """
-    else: 
-        CSS_STYLING = data.get('css_styling')
+    else:
+        CSS_STYLING = data.get("css_styling")
 MODEL = genanki.Model(
-MODEL_NO,
-'Simple Model',
-fields=[
-{'name': 'Kanji'},
-{'name': 'Meaning'},
-],
-templates=[
-{
-    'name': 'Card 1',
-    'qfmt': '<span class="kanji">{{Kanji}}</span>',
-    'afmt': '{{Meaning}}',
-} 
-],
-css=CSS_STYLING
-
+    MODEL_NO,
+    "Simple Model",
+    fields=[
+        {"name": "Kanji"},
+        {"name": "Meaning"},
+    ],
+    templates=[
+        {
+            "name": "Card 1",
+            "qfmt": '<span class="kanji">{{Kanji}}</span>',
+            "afmt": "{{Meaning}}",
+        }
+    ],
+    css=CSS_STYLING,
 )
 
 
-class Deck(Deck):
-    def __init__(self, deck_id=None, name=None, description='', jlpt_level=None, heisig=None): 
+class MirinDeck(Deck):
+    def __init__(
+        self, deck_id=None, name=None, description="", jlpt_level=None, heisig=None
+    ):
         super().__init__(deck_id=deck_id, name=name, description=description)
         self.jlpt_level = jlpt_level
         self.heisig = heisig
 
-    def add_card_helper(self, data): 
+    def add_card_helper(self, data):
         """
         Args:
-            Takes a dictionary of data. This is the request from kanjiapi. 
-        Returns: 
-            Nothing 
+            Takes a dictionary of data. This is the request from kanjiapi.
+        Returns:
+            Nothing
         """
-        kanji = data.get('kanji')
+        kanji = data.get("kanji")
         meanings = ""
         on_readings = ""
         kun_readings = ""
-        meanings = ', '.join(data["meanings"])
-        on_readings = ', '.join(data["on_readings"])
-        kun_readings = ', '.join(data["kun_readings"])
-        
-        time.sleep(2) # Waiting 2 seconds is probably fine and permissable
+        meanings = ", ".join(data["meanings"])
+        on_readings = ", ".join(data["on_readings"])
+        kun_readings = ", ".join(data["kun_readings"])
+        time.sleep(2)  # Waiting 2 seconds is probably fine and permissable
         base = f"""
         on reading(s): {on_readings}
         kun readings: {kun_readings}
         meaning(s): {meanings}
         """
-        if self.heisig and data.get('heisig_en'):
+        if self.heisig and data.get("heisig_en"):
             # logging.info(data.get('heisig_en'))
             base += f"\nHeisig keyword: {data.get('heisig_en')}"
         # Uses the genanki note function and then uses the inherited add note method to add the note to the Deck.
-        my_note = genanki.Note(
-            model=MODEL,
-            fields=[kanji, base])
+        my_note = genanki.Note(model=MODEL, fields=[kanji, base])
         self.add_note(my_note)
 
-    def _make(self, path, deck:Deck, jlpt, threshold):
+    def _make(self, path, deck: Deck, jlpt, threshold):
         """
         Args:
-            Takes a sub path to a subdirectory of the /databases/ directory. 
-            With this it will go through each of the databases and make a deck with it. 
+            Takes a sub path to a subdirectory of the /databases/ directory.
+            With this it will go through each of the databases and make a deck with it.
 
         """
-        with open(path.path, encoding='utf-8-sig') as file: 
+        with open(path.path, encoding="utf-8-sig") as file:
             count = 0
-            #logging.info(path.path)
+            # logging.info(path.path)
             kanji_database = json.load(file)
             for kanji, frequency in kanji_database.items():
-                if (jlpt is not None) and frequency >= threshold: 
+                if (jlpt is not None) and frequency >= threshold:
                     r = Kanji.search_kanji(kanji).json()
-                    if (jlpt is not None and r.get('jlpt') is not None) and int(r.get('jlpt')) <= jlpt:
+                    if (jlpt is not None and r.get("jlpt") is not None) and int(
+                        r.get("jlpt")
+                    ) <= jlpt:
                         logging.info("JLPT level is within the treshold.")
                         # So in this case, the JLPT flag isn't None, and it is above the threshold and it's below the upper bound of JLPT.
-                        deck.add_card_helper(r.get('kanji'))
+                        deck.add_card_helper(r.get("kanji"))
                         continue
-                    else: 
+                    else:
                         logging.info("JLPT level is NOT within the treshold.")
                         continue
-
                     # in this case the JLPT level is None, so just add as normal since it's above the treshold.
                 elif frequency >= threshold:
                     deck.add_card_helper(Kanji.search_kanji(kanji).json())
-                else: 
+
+                else:
                     # Doesn't qualify by any criteria
-                    continue 
+                    continue
+
         return deck
--- dashi/search.py
+++ dashi/search.py
@@ -1,33 +1,37 @@
 import requests
-import json 
+import json
 import re
+
 # -*- coding: utf-8 -*-
 
 BASE_URL = "https://kanjiapi.dev/v1/kanji/"
-class Kanji: 
+
+
+class Kanji:
     @classmethod
     def search_kanji(cls, kanji):
-        encoded = (BASE_URL+kanji).encode('utf-8') # This looks like b'https://kanjiapi.dev/v1/kanji/\xe5\xa0\x82' etc
-        decoded = encoded.decode('utf-8')
-        # if response.status_code == 200: 
+        encoded = (BASE_URL + kanji).encode(
+            "utf-8"
+        )  # This looks like b'https://kanjiapi.dev/v1/kanji/\xe5\xa0\x82' etc
+        decoded = encoded.decode("utf-8")
+        # if response.status_code == 200:
         #     logging.info("Successful, status code {}".format(response.status_code))
-        # else: 
+        # else:
         #     logging.info("Unsuccessful error code {}".format(response.status_code))
-        
+
         return requests.get(decoded)
 
     @classmethod
-    def is_kanji(cls, kanji: str) -> bool: 
+    def is_kanji(cls, kanji: str) -> bool:
         """
-        Args: 
-            kanji: this is the unencoded "kanji" character. 
-        Returns: 
+        Args:
+            kanji: this is the unencoded "kanji" character.
+        Returns:
             True if it is within the unicode range of kanji
-            False if it is not. 
+            False if it is not.
 
         """
 
         if re.match("([一-龯])", kanji):
             return True
         return False
-
